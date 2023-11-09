5eaf0900e2b10b1e77ce0dd7d2298223cb71276b
GONDAL-OFFICIAL <gondalofficial307@gmail.com>
2023-11-09 06:38:04 +0500

    ATTEMPT#1

--- formcode.js
+++ formcode.js
@@ -1,9 +1,9 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz_97RDsNphlswxrVgzqFF3lqGdxWKqlJTJxsHF10h5ZIPVPqD7Jy9mbQ7bHfHdNLg2Gw/exec'
-const form = document.forms['submit-to-google-sheet']
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwyDMB6GStUln-bdaE4jidAkPJzXeHUkPNZod2CP5aUIDPtniUibwQ-cRq9WneQ4ZIx/exec'
+  const form = document.forms['submit-to-google-sheet']
 
-form.addEventListener('submit', e => {
+  form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
-})
+  })
