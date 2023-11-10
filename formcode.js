3f55f331630e6cc87f7d87cf18c34b8a00915b69
GONDAL-OFFICIAL <gondalofficial307@gmail.com>
2023-11-10 07:16:22 +0500

    SOME HOW>>>>

--- formcode.js
+++ formcode.js
@@ -1,9 +1,20 @@
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwyDMB6GStUln-bdaE4jidAkPJzXeHUkPNZod2CP5aUIDPtniUibwQ-cRq9WneQ4ZIx/exec'
-  const form = document.forms['submit-to-google-sheet']
+  // const scriptURL = 'https://script.google.com/macros/s/AKfycbwyDMB6GStUln-bdaE4jidAkPJzXeHUkPNZod2CP5aUIDPtniUibwQ-cRq9WneQ4ZIx/exec'
+  // const form = document.forms['submit-to-google-sheet']
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+  // form.addEventListener('submit', e => {
+  //   e.preventDefault()
+  //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  //     .then(response => console.log('Success!', response))
+  //     .catch(error => console.error('Error!', error.message))
+  // })
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwyDMB6GStUln-bdaE4jidAkPJzXeHUkPNZod2CP5aUIDPtniUibwQ-cRq9WneQ4ZIx/exec'
+
+        const form = document.forms['submit-to-google-sheet']
+    
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("DAGAR DATA ENTRY SUCCUSSFULL."))
+                .then(() => { window.location.reload(); })
+                .catch(error => console.error('Error!', error.message))
+            })
